431ececeb87aba900d9c12159e230943aa7c079b
Hinanshi Suthar <hinanshis4@gmail.com>
2022-05-08 01:59:32 +0530

    feat: setup up routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
-import { Aside } from "./components/Aside/Aside";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Home } from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import { Watchlist } from "./pages/Watch Later/Watchlist";
+import { Playlist } from "./pages/Playlist/Playlist";
+import { History } from "./pages/History/History";
+import { LikedVideos } from "./pages/Liked Videos/LikedVideos";
+import { Aside } from "./components/Aside/Aside";
 
 function App() {
   return (
@@ -8,6 +14,13 @@ function App() {
       <Navbar />
       <div className="grid grid-20-80">
         <Aside />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/watchlater" element={<Watchlist />} />
+          <Route path="/playlist" element={<Playlist />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/liked" element={<LikedVideos />} />
+        </Routes>
       </div>
     </div>
   );
