9189c1ed12091cfea9d0e99b3a058c82cdb72665
Hinanshi Suthar <hinanshis4@gmail.com>
2022-05-07 17:31:26 +0530

    style: helpers for utility classes and icons

---
+++ src/components/utilities/icons.jsx
@@ -0,0 +1,6 @@
+import {AiOutlineMenu, AiFillHome, AiFillPlayCircle, AiFillLike, AiOutlineHistory} from "react-icons/ai"
+import {BsSunFill ,BsSearch} from 'react-icons/bs'
+import {VscAccount} from 'react-icons/vsc'
+import {MdOutlineWatchLater} from 'react-icons/md'
+
+export {AiOutlineMenu, BsSunFill, VscAccount, BsSearch, AiFillHome, MdOutlineWatchLater, AiFillPlayCircle, AiFillLike, AiOutlineHistory};
