8709130f803e6ffbedfe8b0850c08f2055215a4d
Hinanshi Suthar <hinanshis4@gmail.com>
2022-05-09 00:48:37 +0530

    feat: auth set up

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
 import { DataProvider } from "./context/data-context";
+import { AuthProvider } from "./context/auth-context";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <DataProvider>
-      <Router>
-        <App />
-      </Router>
-    </DataProvider>
+    <AuthProvider>
+      <DataProvider>
+        <Router>
+          <App />
+        </Router>
+      </DataProvider>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,52 @@
+import { useAuth } from "../../context/auth-context";
+import "../../components/utilities/helper.css";
+import { useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  const { token, setToken, user, setUser } = useAuth();
+  const { firstName, lastName, email } = user;
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("login");
+    setToken("");
+    setUser();
+    setTimeout(() => console.log("logged out"), 1000);
+    navigate("/");
+  };
+
+  return (
+    <div className="wrapper center">
+      <div className="wide-con auth-con signup-con p-1 flex-col-sb">
+        <h1 className="text-center">User Profile</h1>
+        <div className="credentials-con flex-col-sb-start width-100 p-sm">
+          <h4>Data</h4>
+          <div className="flex-row-sb width-100 py-sm">
+            <div className="flex-col-sb-start">
+              <p>Full Name</p>
+              <p>Registered Email</p>
+            </div>
+            <div className="flex-col-sb-start">
+              <p>
+                {firstName} {lastName}
+              </p>
+              <p>{email}</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex-col-sb-start btn-con width-100">
+          <h4 className="p-sm">Settings</h4>
+          <button
+            className="btn btn-primary text-regular text-capitalize m-sm width-100"
+            onClick={logoutHandler}
+          >
+            Log Out
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Profile };
