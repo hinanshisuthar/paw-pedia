815889e68a0bb49383e8f9c430eb5dcaeddc133c
Hinanshi Suthar <hinanshis4@gmail.com>
2022-05-08 05:47:59 +0530

    feat: video listing

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,56 +1,20 @@
 import { Categories } from "../../components/Categories/Categories";
 import "./Home.css";
-import { BsThreeDotsVertical } from "../../components/utilities/icons";
+import { VideoCard } from "./components/VIdeoCard";
+import { useData } from "../../context/data-context";
 
 const Home = () => {
+  const { videos, category } = useData();
+
   return (
     <div className="video-listing">
       <div className="categories-con">
         <Categories />
       </div>
       <div className="video-card-con p-1 ml-sm">
-        <div className="responsive-img-container video-card">
-          <img
-            src="https://i.ytimg.com/vi/wtH-hdOF1uA/maxresdefault.jpg"
-            alt="hello"
-            className="img-responsive"
-          />
-          <div className="flex-row-sb">
-            <div className="p-1 flex-col-sb-start">
-              <h3>Video Name</h3>
-              <p>Video Creator</p>
-            </div>
-            <BsThreeDotsVertical className="option-icon" size={25} />
-          </div>
-        </div>
-        <div className="responsive-img-container video-card">
-          <img
-            src="https://i.ytimg.com/vi/wtH-hdOF1uA/maxresdefault.jpg"
-            alt="hello"
-            className="img-responsive"
-          />
-          <div className="flex-row-sb">
-            <div className="p-1 flex-col-sb-start">
-              <h3>Video Name</h3>
-              <p>Video Creator</p>
-            </div>
-            <BsThreeDotsVertical className="option-icon" size={25} />
-          </div>
-        </div>
-        <div className="responsive-img-container video-card">
-          <img
-            src="https://i.ytimg.com/vi/wtH-hdOF1uA/maxresdefault.jpg"
-            alt="hello"
-            className="img-responsive"
-          />
-          <div className="flex-row-sb">
-            <div className="p-1 flex-col-sb-start">
-              <h3>Video Name</h3>
-              <p>Video Creator</p>
-            </div>
-            <BsThreeDotsVertical className="option-icon" size={25} />
-          </div>
-        </div>
+        {videos.map((video) => (
+          <VideoCard key={video._id} video={video} />
+        ))}
       </div>
     </div>
   );
