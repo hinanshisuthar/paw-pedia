a6267f1826d9c5448cc9712a26fc418582d63492
Hinanshi Suthar <hinanshis4@gmail.com>
2022-05-08 05:45:57 +0530

    feat: categories listing

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,19 +1,23 @@
-import './Categories.css'
+import { useData } from "../../context/data-context";
+import "./Categories.css";
 
 const Categories = () => {
-    return (
-        <div className="flex-row-sb-start p-1">
-            <button className="text-regular chips m-sm p-sm">
-                Healthcare
-            </button>
-            <button className="text-regular chips m-sm p-sm">
+  const { category } = useData();
+
+  return (
+    <div className="flex-row-sb-start p-1">
+      {category.map(({_id, categoryName}) => (
+        <button key={_id} className="text-regular chips m-sm p-sm">{categoryName}</button>
+      ))}
+
+      {/* <button className="text-regular chips m-sm p-sm">
                 Training
             </button>
             <button className="text-regular chips m-sm p-sm">
                 Adoption
-            </button>
-        </div>
-    )
-}
+            </button> */}
+    </div>
+  );
+};
 
-export {Categories}
+export { Categories };
