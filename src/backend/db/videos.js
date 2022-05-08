11c7d5b4f9c407fcc3ba16d2f82e54a60d781e0f
Hinanshi Suthar <hinanshis4@gmail.com>
2022-05-08 05:45:35 +0530

    feat: added db for videos

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,93 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "qOoCIlYxLb4",
+    title: "Dog Showing All Training Skills",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Dog Showing All Training Skills - Our 12 weeks old Labrador Puppy  learning and performing basic training commands like Sit, Stay, Shake, High 5, Lie Down, Crawl, Give Paw, Spin, Bark and other training. Dog showing training skills tricks",
+    creator: "Furry Friend",
+    category: 'Training',
+    date: 'Jan 5 2017',
+    views: '7,869,953',
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "1oDGa2yPb2g",
+    title: "DOG TRAINING FUNDAMENTALS",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Dogumentary TV has teamed up with Oscar Mora of Elevated Canine Academy to bring you Dog Training Fundamentals.",
+    creator: "Dogumentaty TV",
+    category: 'Training',
+    date: 'May 18 2019',
+    views: '2,136,769',
+  },
+  {
+    _id: "JhKfEOLmDAU",
+    title: "7 TIPS to CARE for Your DOG'S HEALTH ",
+    description:
+      "Do you want to know how to keep your dog healthy? In this AnimalWised video, in collaboration with iNetPet (https://inetpet.com/home) we provide 7 tips to take care of your dog's health. We adore our furry companions and want them to stay by our side as long as possible. ",
+    creator: "Animal Wised",
+    category: 'Healthcare',
+    date: 'Sept 6 2021',
+    views: '2,249',
+  },
+  {
+    _id: "FA4R9TBWOIU",
+    title: "10 Healthy Dog Tips",
+    description:
+      "Caring for dogs can be ruff for new pet parents, especially if you’re one of those pet people who is absolutely committed to raising a happy, healthy dog in a world full of all kinds of misleading dog info.",
+    creator: "Chewy",
+    category: 'Healthcare',
+    date: 'Mar 9, 2019',
+    views: '12,444',
+  },
+  {
+    _id: "07d2dXHYb94",
+    title: "Pip | A Short Animated Film",
+    description:
+      "Dog’s heroics will make you cry! Donate at https://www.guidedogs.org/pip",
+    creator: "Southeastern Guide Dogs",
+    category: 'Films and Animation',
+    date: 'Oct 25 2018',
+    views: '388,989,152',
+  },
+  {
+    _id: "YGDL70q7fKA",
+    title: "Barfi | Part 1: Tu Jo Mila",
+    description:
+      "Barfi tells the story of a loner and depressed man Lucky whose life changes for good when a furry animal enters his life.  ",
+    creator: "Harsh Beniwal",
+    category: 'Films and Animation',
+    date: 'Mar 11 2022',
+    views: '14,323,587',
+  },
+  {
+    _id: "zwzMy7WfGlY",
+    title: "How To Choose A Shelter Dog",
+    description:
+      "Go to an animal shelter and you’ll likely find that there’s no shortage of canines waiting to go to their furever homes. The typical shelter will have a diverse selection of dogs: large, small, long-haired, short-haired, high-strung, laid back, purebred, crossbred, and everything in between. So how do you sort through the differences to pick the right pooch? ",
+    creator: "Animal Facts",
+    category: 'Adoption',
+    date: 'Aug 10 2015',
+    views: '60,170',
+  },
+  {
+    _id: "z0AUfyoPu64",
+    title: "THINGS YOU *MUST* KNOW BEFORE ADOPTING A DOG",
+    description:
+      "Welcome to the ultimate guide to adopting your first dog!",
+    creator: "Julia Lee",
+    category: 'Adoption',
+    date: 'Jan 11 2017',
+    views: '53,787',
+  },
+  {
+    _id: "YQDDm9HLkV4",
+    title: "I Adopted EVERY Dog In A Dog Shelter",
+    description:
+      "Every family who adopted a dog was vetted and approved by the animal shelter, im so happy these dogs get to live with loving families! :)))))",
+    creator: "Mr Beast",
+    category: 'Make My Day',
+    date: 'Feb 20 2020',
+    views: '119,716,115',
   },
 ];
