51278b851dc98eb937b6b7a1dd33a88d0d6bba41
Hinanshi Suthar <hinanshis4@gmail.com>
2022-05-09 05:31:30 +0530

    feat: added icons

--- src/components/utilities/icons.jsx
+++ src/components/utilities/icons.jsx
@@ -1,6 +1,6 @@
-import {AiOutlineMenu, AiFillHome, AiFillPlayCircle, AiFillLike, AiOutlineHistory} from "react-icons/ai"
+import {AiOutlineMenu, AiFillHome, AiFillPlayCircle, AiFillLike, AiOutlineHistory, AiOutlineLike} from "react-icons/ai"
 import {BsSunFill ,BsSearch, BsThreeDotsVertical} from 'react-icons/bs'
 import {VscAccount} from 'react-icons/vsc'
 import {MdOutlineWatchLater, MdWatchLater} from 'react-icons/md'
 
-export {AiOutlineMenu, BsSunFill, VscAccount, BsSearch, AiFillHome, MdOutlineWatchLater, AiFillPlayCircle, AiFillLike, AiOutlineHistory, BsThreeDotsVertical, MdWatchLater};
+export {AiOutlineMenu, BsSunFill, VscAccount, BsSearch, AiFillHome, MdOutlineWatchLater, AiFillPlayCircle, AiFillLike, AiOutlineHistory, BsThreeDotsVertical, MdWatchLater, AiOutlineLike};
